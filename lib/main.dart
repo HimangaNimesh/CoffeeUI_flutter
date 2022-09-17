95a2dac7714804471584e6cb9ffb9d3d3444f140
HimangaNimesh <himanganimesh2@gmail.com>
2022-09-17 23:39:02 +0530

    Final result

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:coffeeui/pages/homepage.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          brightness: Brightness.dark,
+        primarySwatch: Colors.orange,
+      ),
+      home: HomePage(),
+    );
+  }
+}
